856496e0e430dd2fcbf88d8548cb2dd85fcc9652
wavydave <davlog93090@gmail.com>
2015-10-22 08:58:52 -0600

    sixth

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var http = require('http');
 var fs = require('fs');
 var bodyParser = require('body-parser');
-var db = require('./model/db');
+//var db = require('./model/db');
 var blogModel = require('./model/post');
 var blogRoutes = require('./routes/post');
 
@@ -11,11 +11,13 @@ var app = express();
 
 var router = express.Router();              
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use('/api', router);
 
 app.use(express.static('public'));
 
-app.use('/api/blog', blogRoutes);
+//app.use('/api/blog', blogRoutes);
 
 app.get('/', function(req, res){
     res.readFile('index.html')
@@ -23,5 +25,6 @@ app.get('/', function(req, res){
 
 
 
-app.listen(3000);
-console.log("Get this Sh!& to work!");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
