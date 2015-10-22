d9eabea3d6ae9529f42dbe10eaf10bc2b3040f21
wavydave <davlog93090@gmail.com>
2015-10-22 09:08:54 -0600

    working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var http = require('http');
 var fs = require('fs');
 var bodyParser = require('body-parser');
-//var db = require('./model/db');
+var db = require('./model/db');
 var blogModel = require('./model/post');
 var blogRoutes = require('./routes/post');
 
@@ -17,7 +17,7 @@ app.use('/api', router);
 
 app.use(express.static('public'));
 
-//app.use('/api/blog', blogRoutes);
+app.use('/api/blog', blogRoutes);
 
 app.get('/', function(req, res){
     res.readFile('index.html')
