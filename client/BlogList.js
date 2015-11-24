ef917754be20a38ee91c887f98d5fef59b81293b
wavydave <davlog93090@gmail.com>
2015-11-24 12:50:04 -0700

    Pictures, order, Users

--- client/BlogList.js
+++ client/BlogList.js
@@ -3,40 +3,32 @@ var BlogComment = require('./BlogComment');
 var prettydate = require('pretty-date');
 
 var BlogList = React.createClass({
-	
+
 	render: function(){
 		var self = this;
-		var blogSort = this.props.data.sort(function(a, b){
-			var x = a.date, y = b.date;
-			return x < y ? -1 : x > y ? 1 : 0;
-		});
 		var blogData = this.props.data.map(function(blog){
-			var comments = blog.comments.map(function(comment){
-				window.com = comment.body;
-				window.user = comment.user;
-			})
-
-
-			if(blog.comments.length > 0){
-				var comments = blog.comments.map(function(c){
-					var newDate = prettydate.format(new Date(c.date))
-					return (
-						<p> {c.body} <strong>{newDate}</strong> </p>
+			if(blog.comments.length>0){
+				var comments = blog.comments.map(function(comment){
+					var newDate = prettydate.format(new Date(comment.date))
+				return (
+					<p> {comment.body} <strong>{newDate}</strong></p>
 						)
-				})	
+
+				}).reverse();
 			} else {
-				var comments = "No Comments Yet.."
+				var comments = "Nothing to be said"
 			}
 			return (
 				<div>
-					<h3> {blog.title} </h3>
-					<p> {blog.body} </p>
-					<p> {blog.date} </p>
-					{comments}					
-					<BlogComment blogId={blog._id} onPost={self.props.newData}/>
+						<h3> {blog.title} </h3>
+						<p> {blog.body} </p>
+						<p> {blog.date} </p>
+							{comments}					
+						<BlogComment blogId={blog._id} onPost={self.props.newData}/>
 				</div>
 				)
-		});
+		}).reverse();
+
 		return (
 			<div>
 				<h1> Blogs </h1>
@@ -44,8 +36,9 @@ var BlogList = React.createClass({
 					{blogData}
 				</ul>
 			</div>
-			);
+			)
 	}
+	
 });
 
 module.exports = BlogList;
